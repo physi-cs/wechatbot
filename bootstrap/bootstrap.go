3a84948f7bbcb0bd9cff81af1e5c5792e174698d
SHwen <919836750@qq.com>
2023-04-21 10:29:41 +0800

    取消热登录功能

--- bootstrap/bootstrap.go
+++ bootstrap/bootstrap.go
@@ -20,17 +20,17 @@ func Run() {
 	bot.UUIDCallback = openwechat.PrintlnQrcodeUrl
 
 	// 创建热存储容器对象
-	reloadStorage := openwechat.NewJsonFileHotReloadStorage("storage.json")
+	//reloadStorage := openwechat.NewJsonFileHotReloadStorage("storage.json")
 
 	// 执行热登录
-	err := bot.HotLogin(reloadStorage)
+	//err := bot.HotLogin(reloadStorage)
+	//if err != nil {
+	err := bot.Login()
 	if err != nil {
-		err := bot.Login()
-		if err != nil {
-			log.Printf("login error: %v \n", err)
-			return
-		}
+		log.Printf("login error: %v \n", err)
+		return
 	}
+	//}
 	//异步执行一个定时清空历史消息的函数，周期为24小时
 	go glm.ClearHistoryStackSlice()
 	// 阻塞主goroutine, 直到发生异常或者用户主动退出
